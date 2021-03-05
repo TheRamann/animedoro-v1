0f3b9d362562cfc012071bdb1aee318f762f7f76
TheRamann <69319475+TheRamann@users.noreply.github.com>
2021-03-05 15:39:33 +0530

    Completely rewrote timer codes

--- public/countdown.js
+++ public/countdown.js
@@ -4,28 +4,27 @@ var runningornot = false;
 // eslint-disable-next-line no-unused-vars
 function startcount() {
     if(runningornot === false){
-        runningornot = true;
+        runningornot = true
         const startingMinutes = 40;
         let time = startingMinutes * 60;
-        let endtime = false;
         const countdownEl = document.getElementById('countdown');
-    
-        setInterval(()=>{
+        var AfterStarting = setInterval(()=>{
             const minutes = Math.floor(time / 60);
             let seconds = time % 60;
-            if(minutes == '0' && seconds == '0'){ 
-                if(endtime == false){
-                    countdownEl.innerHTML = `Time To Watch Anime`
-                    endtime = true
-                    runningornot = false;
-                    alert("Time To Watch Anime")
-                }
-                return;
-                }
             seconds = seconds < 10 ? '0' + seconds : seconds;
             countdownEl.innerHTML = `${minutes}: ${seconds}`
             time--;
-        }, 1000);
+            if(minutes == 0 && seconds == 0){
+                countdownEl.innerHTML = "Time To Watch Anime"
+                clearInterval(AfterStarting)
+                alert("Time To Watch Anime")
+            }
+        }
+        , 1000);
+        document.getElementById('pauseButton').addEventListener("click", function () {
+            clearInterval(AfterStarting)
+        })
+        
     }
 
 }
