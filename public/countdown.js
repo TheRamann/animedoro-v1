1f35ca955fb42d865427a77e7da2965915864d3e
TheRamann <69319475+TheRamann@users.noreply.github.com>
2021-03-05 16:01:23 +0530

    added a pause button

--- public/countdown.js
+++ public/countdown.js
@@ -1,26 +1,42 @@
 /* eslint-disable eqeqeq */
 var runningornot = false;
-
+var isPaused;
 // eslint-disable-next-line no-unused-vars
 function startcount() {
     if(runningornot === false){
+        isPaused = false;
         runningornot = true
-        const startingMinutes = 40;
+        const startingMinutes = 0.05;
         let time = startingMinutes * 60;
         let minutes;
         let seconds
         const countdownEl = document.getElementById('countdown');
-        var AfterStarting = setInterval(()=>{
-            minutes = Math.floor(time / 60);
-            seconds = time % 60;
-            seconds = seconds < 10 ? '0' + seconds : seconds;
-            countdownEl.innerHTML = `${minutes}: ${seconds}`
-            time--;
-            if(minutes == 0 && seconds == 0){
-                countdownEl.innerHTML = "Time To Watch Anime"
-                clearInterval(AfterStarting)
-                alert("Time To Watch Anime")
+        document.getElementById("pauseButton").addEventListener('click', function () {
+            console.log("working?")
+            if(isPaused == true){
+                isPaused = false
             }
+            if(isPaused == false){
+                isPaused = true
+            }
+        })
+        var AfterStarting = setInterval(()=>{
+                do { 
+                    
+                } while (isPaused == true);
+            if(isPaused == false){
+                minutes = Math.floor(time / 60);
+                seconds = time % 60;
+                seconds = seconds < 10 ? '0' + seconds : seconds;
+                countdownEl.innerHTML = `${minutes}: ${seconds}`
+                time--;
+                if(minutes == 0 && seconds == 0){
+                    countdownEl.innerHTML = "Time To Watch Anime"
+                    clearInterval(AfterStarting)
+                    alert("Time To Watch Anime")
+                }
+            }
+            
         }
         , 1000);
         
