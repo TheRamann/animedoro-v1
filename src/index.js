39d4d3765ab9ad12e8fd95e92120c23a5cfc2664
TheRamann <69319475+TheRamann@users.noreply.github.com>
2021-03-05 15:19:45 +0530

    removed Progress Bars

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AllBars from './AllBars';
+// import AllBars from './AllBars';
 
 ReactDOM.render(
   <React.StrictMode>
-    <br />
-    <AllBars />
   </React.StrictMode>,
   document.getElementById('progressBar')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
